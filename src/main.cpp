1df2387b8708d0b268e8dee4a76936a1bd27224f
Lucas <lucas.a.brand@gmail.com>
2026-01-31 20:28:17 -0500

    For some reason the previous commit didn't work, this is now the completed foundation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include "raylib.h"
 
 int main() {
-    InitWindow(800, 450, "My New Game");
+    InitWindow(1920, 1200, "Cyphon");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello, Raylib!", 190, 200, 20, BLACK);
+
+        ClearBackground(BLACK);
+
         EndDrawing();
     }
 
